0b09b223e46a7244c9e02ec2bd3b92c4ab830ef5
Nikolay-Lyasota <nikolaylyasota@gmail.com>
2021-06-13 21:06:52 +0300

    init commit

--- src/main/java/koschei/models/Island2.java
+++ src/main/java/koschei/models/Island2.java
@@ -1,9 +1,18 @@
 package koschei.models;
 
+import koschei.config.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Island2 {
 
     private Wood3 wood;
 
+    @Autowired
     public Island2(Wood3 wood) {
         this.wood = wood;
     }
